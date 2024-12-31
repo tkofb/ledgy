d3f847a50acebdfdebaf6b04bff2ac6cc84ee741
tkofb <benard.kihiuria@gmail.com>
2024-12-31 09:12:05 -0500

    Start on server code

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,52 @@
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import "./Login.css";
+import axios from "axios";
 
 const Login: React.FC = () => {
   const [activeTab, setActiveTab] = useState<"login" | "create">("login");
+
+  // Login Refs
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  // SignUp Refs
+  const usernameSignUpRef = useRef<HTMLInputElement>(null);
+  const emailSignUpRef = useRef<HTMLInputElement>(null);
+  const passwordSignUpRef = useRef<HTMLInputElement>(null);
+
+  async function handleSignUp(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const username = usernameSignUpRef.current?.value;
+    const email = emailSignUpRef.current?.value;
+    const password = passwordSignUpRef.current?.value;
+
+    const variables = {
+      username,
+      email,
+      password,
+    };
+
+    try {
+      const res = await axios.post("http://localhost:3000/register", variables);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log("signUp");
+    console.log(username, email, password);
+  }
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log("login");
+    console.log(email, password);
+  };
 
   return (
     <div className="containerHolder">
@@ -31,15 +75,21 @@ const Login: React.FC = () => {
           {activeTab === "login" && (
             <div className="content active">
               <h2 className="bold">login</h2>
-              <form>
+              <form onSubmit={handleLogin}>
                 <label htmlFor="email">email:</label>
-                <input type="email" id="email" placeholder="enter your email" />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="enter your email"
+                  ref={emailRef}
+                />
 
                 <label htmlFor="password">password:</label>
                 <input
                   type="password"
                   id="password"
                   placeholder="enter your password"
+                  ref={passwordRef}
                 />
 
                 <button className="create bold-button" type="submit">
@@ -52,12 +102,13 @@ const Login: React.FC = () => {
           {activeTab === "create" && (
             <div className="content active">
               <h2 className="bold">create account</h2>
-              <form>
+              <form onSubmit={handleSignUp}>
                 <label htmlFor="username">username:</label>
                 <input
                   type="text"
                   id="username"
                   placeholder="enter your username"
+                  ref={usernameSignUpRef}
                 />
 
                 <label htmlFor="email-create">email:</label>
@@ -65,6 +116,7 @@ const Login: React.FC = () => {
                   type="email"
                   id="email-create"
                   placeholder="enter your email"
+                  ref={emailSignUpRef}
                 />
 
                 <label htmlFor="password-create">password:</label>
@@ -72,6 +124,7 @@ const Login: React.FC = () => {
                   type="password"
                   id="password-create"
                   placeholder="create a password"
+                  ref={passwordSignUpRef}
                 />
 
                 <button className="bold-button submit-button" type="submit">
